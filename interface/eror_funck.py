b13e718fd741f9c9a97364b05beac7df9b034cdf
filip <filipp.vezhnpvets@yandex.ru>
2022-10-11 00:29:32 +0300

    Первый день работы (страшно)

---
+++ interface/eror_funck.py
@@ -0,0 +1,15 @@
+
+class ErorInterface(Exception):
+    pass
+
+
+class CommandNotFound(ErorInterface):
+    pass
+
+
+# class NoFavoriteTracks():
+#     pass
+#
+#
+# class InvalidUrl():
+#     pass
